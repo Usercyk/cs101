b9cd2cc9b2c733329a545338c4bba4d9589f719a
UserCyk <flashboy2017@163.com>
2024-09-22 21:57:07 +0800

    ac!

--- Daily/September/0919/solve.py
+++ Daily/September/0919/solve.py
@@ -1,16 +1,17 @@
-# WA了……实在是不知道怎么过，难绷
 from math import sqrt
 
 
 for _ in range(int(input())):
     a, b, c = map(float, input().split())
-    r = (-b/(2*a))
+    if b == -0.0:
+        b = -b
+    r = -b/(2*a)
     delta = b*b-4*a*c
     if delta == 0:
-        print(f"x1==x2=={'%.5f' % r}")
+        print(f"x1=x2={r:.5f}")
     elif delta > 0:
         print(
-            f"x1=={'%.5f' % (r+sqrt(delta)/(2*a))};x2=={'%.5f' % (r-sqrt(delta)/(2*a))}")
+            f"x1={(r+sqrt(delta)/(2*a)):.5f};x2={(r-sqrt(delta)/(2*a)):.5f}")
     else:
         print(
-            f"x1=={'%.5f' % r}+{'%.5f' % (sqrt(-delta)/(2*a))}i;x1=={'%.5f' % r}-{'%.5f' % (sqrt(-delta)/(2*a))}i")
+            f"x1={r:.5f}+{(sqrt(-delta)/(2*a)):.5f}i;x2={r:.5f}-{(sqrt(-delta)/(2*a)):.5f}i")
